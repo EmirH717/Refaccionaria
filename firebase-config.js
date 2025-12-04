2dfd3368c47a9d0999d4b435d6de3722acc62d88
Lisander Emir Hernández Hernández <lisanderemirhernandez23@cbtis179.edu.mx>
2025-12-03 23:29:27 -0600

    Fix firebase config and restore .gitignore

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,15 @@
-module.exports = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
+// Realtime Database
+const db = admin.database();
+
+module.exports = { admin, db };
